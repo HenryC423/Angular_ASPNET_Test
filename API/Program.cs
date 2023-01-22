bf21eb440cebe52f0a86f4c96640695b83ba0251
Henry Caldwell <henrycaldwell423@gmail.com>
2023-01-22 02:29:36 -0500

    Angular components communicate with API endpoints

--- API/Program.cs
+++ API/Program.cs
@@ -14,11 +14,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
-var app = builder.Build();
 
+var app = builder.Build();
 // Configure the HTTP request pipeline
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
-
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
